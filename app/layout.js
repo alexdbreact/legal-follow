018bc9bad38476a1fa14be78c7c3636f60706513
alexdbreact <150357569+alexdbreact@users.noreply.github.com>
2025-04-19 10:18:50 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,12 +11,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
       <body>
         <Navbar/>
         {children}
         <Footer />
         </body>
-    </html>
   );
 }
